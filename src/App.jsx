c6cbc643083a6ad83c6068f8c23b8f1abe1a1cbc
Limb00z <86503203+Limb00z@users.noreply.github.com>
2023-03-22 16:48:27 -0300

    3 files modified, 3 added, 3 deleted

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css';
+import Banner from './components/Banner';
 import Footer from './components/Footer';
 import Header from './components/Header';
 
@@ -6,6 +7,7 @@ function App() {
   return (
     <div>
       <Header/>
+      <Banner/>
       <Footer/>
     </div>
   );
---
+++ src/components/Banner/index.jsx
@@ -0,0 +1,7 @@
+import style from './Banner.module.css'
+
+export default function Banner(){
+  return(
+    <div className={style.banner}></div>
+  )
+}
